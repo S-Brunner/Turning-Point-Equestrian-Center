895c5f6ad8bc9a673cb7bcec229aee7c8f10d6c4
Your Name <you@example.com>
2021-12-07 18:49:01 -0800

    Some Endpoints Added

--- server/Handlers/userHandlers.js
+++ server/Handlers/userHandlers.js
@@ -33,10 +33,61 @@ const addNewUser = async (req, res) => {
         console.log(error);
         res.status(400).json({ status: 400, message: "Somthing went wrong", error})
     }
+}
 
+const getUser = async (req, res) => {
+        const client = new MongoClient(MONGO_URI, options)
+    
+        try {
+            await client.connect();
+    
+            const db = client.db("turningPoint");
+
+            const { _id } = req.params;
+    
+            const result = await db.collection("users").findOne({ _id });
+
+            if(!result){
+                return res.status(400).json({ status: 400, message: "No user found"}); 
+            }
+
+            res.status(200).json({ status: 200, message: "User found :)", data: result });
+
+            client.close();
+        } catch (error) {
+            console.log(error);
+            res.status(400).json({ status: 400, message: "Someothing went wrong getting the user :/"});
+        }
+    }
+
+const deleteUser = async (req, res) => {
+    const client = new MongoClient(MONGO_URI, options)
+
+    try {
+        await client.connect();
+
+        const db = client.db("turningPoint");
+
+        const { _id } = req.params;
+
+        const result = await db.collection("users").deleteOne({ _id })
+
+        if(!result){
+            return res.status(400).json({ status: 400, message: "No user found"}); 
+        }
+
+        res.status(200).json({ status: 200, message: "User Deleted", data: result })
+
+        client.close();
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ status: 400, message: "Someothing went wrong getting the user :/"});
+    }
 
 }
 
 module.exports = {
-    addNewUser
+    addNewUser,
+    getUser,
+    deleteUser
 }
